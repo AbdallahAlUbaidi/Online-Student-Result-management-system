eb2699a77ecd296a0488697a9b62ab0ed380b3c1
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2022-12-09 18:24:11 +0300

    Added user register with email confirmation

---
+++ routers/register/register.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+const errorReport = require('../errorReport')
+const sendConfirmationEmail = require('../emailConfirmation/emailConfirmation').sendConfirmationEmail
+
+
+const user = require('../../models/User')
+const Student = require('../../models/Student')
+const faculty = require('../../models/Faculty')
+
+
+router.get('/' , (req , res)=>
+{
+    res.render('register/register')
+})
+
+router.post('/' , async(req , res)=>
+{
+    try
+    {
+        const newUser = await user.createNewUser(req.body.username , req.body.emailAddress , req.body.password , req.body.role)
+        sendConfirmationEmail(newUser)
+        res.status(201).send('User created successfully')
+    }
+    catch(error)
+    {
+        const errorInfo = errorReport(error)
+        res.status(errorInfo.statusCode).send(errorInfo.errors)
+    }
+})
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,28 +5,39 @@ if(process.env.NODE_ENV !== 'production')
 const express = require("express")
 const epxress_layouts = require("express-ejs-layouts")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const url = require('url')
+
 
 const app = express()
+app.use(bodyParser.urlencoded({limit:"10mb" , extended:false}))
+app.use(express.json())
 
 //Import Routers
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routers/index')
+const registerRouter = require('./routers/register/register')
+const emailConfirmationRouter = require('./routers/emailConfirmation/emailConfirmation').router
+const errorReport = require('./routers/errorReport')
 
 
 //set up express view enigne, layout and the public folder
 app.set('view engine' , 'ejs')
 app.set('views' , __dirname + '/views')
 app.set('layout' , './layouts/layout')
+
 app.use(epxress_layouts)
 app.use(express.static('public'))
 
 //Connecting to database
 mongoose.connect(process.env.DATABASE_URL ,
     ()=>{console.log("Connected Successfully")},
-    (e)=>{console.log(e)}) 
+    (e)=>{errorReport(error)}) 
 
 //Using routers
 app.use('/' , indexRouter)
+app.use('/register' , registerRouter)
+app.use('/emailConfirmation' , emailConfirmationRouter)
 
 
 app.listen(process.env.PORT || 80)

496d7ebcf3a45ed55f37ae28a6b388cd6eec8dc4
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-02-26 05:48:57 +0300

    Grades added evaluation proprty and dynamic score validation based on course type

--- models/Grade.js
+++ models/Grade.js
@@ -1,4 +1,8 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose')
+const Course = require('./Course')
+const Student = require('./Student')
+const Faculty = require('./Faculty')
+
 const gradeSchema = mongoose.Schema({
     student:{
         type:mongoose.SchemaTypes.ObjectId,
@@ -6,29 +10,104 @@ const gradeSchema = mongoose.Schema({
         required:[true , "The graded student was not specified"]
     },
     course:{
-        type:mongoose.SchemaType.ObjectId,
+        type:mongoose.SchemaTypes.ObjectId,
         ref:'Course',
         required:[true , "The course was not specified"]
     },
-    preFinalScore:{
+    gradeStatus:{
+        type:String,
+        lowerCase:true,
+        default:'notGraded',
+        enum:['notGraded' , 'pending' , 'approvedPending' , 'finalized' , 'published']
+
+    },
+    evaluationScore:{
         type:Number,
-        minValue:0
+        min:[0 , 'Must be at least zero'] 
+    },
+    midTermScore:{
+        type:mongoose.Schema.Types.Mixed,
+        validate:{
+            validator:value => typeof value === 'number' || (typeof value === 'string' && value.toUpperCase() === 'ABSENT'),
+            message: props => `${props.value} is an invalid Score Must be a number or "ABSENT"`
+        },
+        toUpperCase:true,
+        min:[0 , 'Must be at least zero']
     },
     finalExamScore:{
-        type:Number,
-        minValue:0
-    },
-    finalGrade:{
-        type:String,
-        enum:['fail' , 'pass' , 'average' , 'good' , 'very good' , 'excellent'],
-        lowerCase:true
+        type:mongoose.Schema.Types.Mixed,
+        validate:{
+            validator:value => typeof value === 'number' || (typeof value === 'string' && value.toUpperCase() === 'ABSENT'),
+            message: props => `${props.value} is an invalid Score Must be a number or "ABSENT"`
+        },
+        toUpperCase:true,
+        min:[0 , 'Must be at least zero']
     }
 
 })
 
-gradeSchema.path('preFinalScore').validate(async score =>{
-    const course = await mongoose.model('Course').findById(this.course)
+
+
+gradeSchema.virtual('preFinalScore')
+.get(async function(){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 30 : 50;
+    const midTermScore = this.midTermScore;
+    if(typeof midTermScore === 'String');
+        midTermScore = 0;
+    const preFinalScore = this.evaluationScore + midTermScore;
+    if(preFinalScore > maxScore)
+        throw new mongoose.Error.ValidationError(`Pre final score must not be higher than ${maxScore}`)
+    return preFinalScore;
+});
+
+gradeSchema.virtual('totalScore')
+.get(function(){
+    const finalExamScore = this.finalExamScore;
+    if(typeof finalExamScore === 'String')
+        finalExamScore = 0;
+    const totalScore = this.preFinalScore + finalExamScore;
+    if(totalScore > 100 || totalScore < 0)
+        throw new mongoose.Error.ValidationError(`Total score must not be higher than 100 or lower than 0`)
+    return totalScore;
+});
+
+gradeSchema.virtual('finalGrade')
+.get(function(){
+    const grades = ['fail' , 'pass' , 'average' , 'good' , 'very good' , 'excellent'];
+    const gradesIndex = this.totalScore === 100 ? 5 : Math.floor(this.totalScore / 10) - 4;
+    return grades[gradesIndex];
     
+})
+
+gradeSchema.path('evaluationScore').validate(async function(score){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 10 : 20;
+    return score <= maxScore
+} , async function(err){
+    const course = await Course.findById(this.course);
+    const maxScore = course.courseType === 'theoretical'? 10 : 20;
+    return `Evaluation score can't be higher than ${maxScore}`
+})
+
+gradeSchema.path('midTermScore').validate(async function(score){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 20 : 30;
+    return score <= maxScore
+} , async function(err){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 20 : 30;
+    return `Mid term Score can't be higher than ${maxScore}`
+})
+
+gradeSchema.path('finalExamScore').validate(async function(score){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 70 : 50;
+    return score <= maxScore
+} , async function(err){
+    const course = await Course.findById(this.course)
+    const maxScore = course.courseType === 'theoretical'? 70 : 50;
+    return `Final exam score can't be higher than ${maxScore}`
 })
 
 

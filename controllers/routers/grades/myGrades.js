8fcc9feca440ea1385d24913c2d3117705abb221
AbdallahAlUbaidi <Abdallah.Alubaidi111@gmail.com>
2023-05-12 17:10:34 +0300

    Added Student Page

---
+++ controllers/routers/grades/myGrades.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {errorReport , renderErrorPage} = require('../../errorReport')
+
+
+//Only Student
+router.get("/" , (req , res) => {
+    try{
+        const student_id = req.info.roleInfo._id;
+        res.render("student/myGrades") , {message:req.flash("message")[0] , messageType:req.flash("messageType")[0] , student_id}
+    }catch(err){
+        const {statusCode , errors , message} = errorReport(err);
+        if(statusCode === 500)
+            renderErrorPage(res , statusCode);
+    }
+});
+
+module.exports = router
